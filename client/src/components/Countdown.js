4b8757fb6138069ce4547589faa31ee1e4beb767
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-15 16:52:39 -0500

    add positioning and styling to the Countdown component

--- client/src/components/Countdown.js
+++ client/src/components/Countdown.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Countdown = () => {
   return (
     <div className='countdown-container'>
-
+      <h2>May 22 - 24, 2022</h2>
     </div>
   )
 }
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,36 +2,39 @@ import React from 'react'
 import { Modal, Button } from 'react-bootstrap';
 import playBtn from '../assets/play_btn.svg';
 import VideoModal from '../components/VideoModal';
+import Countdown from '../components/Countdown';
 
 const Hero = () => {
   const [modalShow, setModalShow] = React.useState(false);
 
   return (
-    <div className='hero'>
-      <div className='color-overlay'>
-        <section className='hero-content'>
-          <header>
-            <h1 className='header'>commencement</h1>
-          </header>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
-          </p>
-          <div className='hero-btn-container'>
-            <button className='register-btn'>Register</button>
-            <button variant="primary" onClick={() => setModalShow(true)}>
-              <img src={playBtn} alt='play button'></img>
-            </button>
-            <VideoModal
-              show={modalShow}
-              onHide={() => setModalShow(false)}
-            />
-          </div>
-        </section>
+    <>
+      <div className='hero'>
+        <div className='color-overlay'>
+          <section className='hero-content'>
+            <header>
+              <h1 className='header'>commencement</h1>
+            </header>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <div className='hero-btn-container'>
+              <button className='register-btn'>Register</button>
+              <button variant="primary" onClick={() => setModalShow(true)}>
+                <img src={playBtn} alt='play button'></img>
+              </button>
+              <VideoModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+              />
+            </div>
+          </section>
+        </div>
       </div>
-
-    </div>
+      <Countdown />
+    </>
   )
 }
 
