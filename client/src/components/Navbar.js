167a2b4515e32f84ee8f07ce4867ca88a7c38b3a
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-14 20:56:01 -0500

    add navbar styling and home icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import logo from '../assets/colby_logo.png'
+import home from '../assets/home.svg'
 
 const NavBar = () => {
   return (
-    <div>
-      <img src={logo} alt="Colby logo"></img>
-      <ul>
-        <li><a href='#'>Home</a></li>
-        <li><a href='#'>Schedule</a></li>
-        <li><a href='#'>Registration</a></li>
-        <li><a href='#'>Share</a></li>
-        <li><a href='#'>Galleries</a></li>
-      </ul>
+    <div className='nav-container'>
+      <nav>
+        <img className='logo' src={logo} alt="Colby logo"></img>
+        <ul className='nav-links-container'>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>Schedule</a></li>
+          <li><a href='#'>Registration</a></li>
+          <li><a href='#'>Share</a></li>
+          <li><a href='#'>Galleries</a></li>
+          <li>
+            <a href='#'><img className='home-icon-link' src={home} alt="Home icon"></img></a>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
