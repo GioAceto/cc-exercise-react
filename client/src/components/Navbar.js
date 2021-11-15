1b9681eb020c18ad366be610db59765845c326b0
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-14 19:21:44 -0500

    add logo and nav links to NavBar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import logo from '../assets/colby_logo.png'
 
 const NavBar = () => {
   return (
     <div>
-
+      <img src={logo} alt="Colby logo"></img>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Schedule</a></li>
+        <li><a href='#'>Registration</a></li>
+        <li><a href='#'>Share</a></li>
+        <li><a href='#'>Galleries</a></li>
+      </ul>
     </div>
   )
 }
