1214a8ef75c4e34debf60f3565a5ab2b63962f81
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-15 11:53:25 -0500

    add content and styling to Home page

--- client/src/components/routes/Home.js
+++ client/src/components/routes/Home.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import '../../styles/styles.scss';
 import NavBar from '../NavBar';
 import Hero from '../Hero';
+import millerMap from '../../assets/miller_map.png';
 
 const Home = () => {
   return (
@@ -14,7 +15,22 @@ const Home = () => {
           <h2>Let the Celebration Begin.</h2>
         </header>
         <main className='main-content'>
-
+          <article>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Modi maxime dolores neque, labore quaerat eligendi eveniet
+              id iure, odio ad accusamus molestiae officiis? Delectus
+              sunt sequi dolores ab sapiente tenetur perferendis eaque
+              voluptate unde quis assumenda possimus maxime eos animi
+              nam corporis autem velit et, dolor a voluptas quia ex
+              cumque. Ut aliquam fuga porro aspernatur sapiente
+              voluptatibus eaque in odio, repudiandae deleniti libero,
+              voluptatum reprehenderit impedit. Harum vero quod, nam
+              totam nobis, in cumque provident impedit quos eveniet
+              corrupti dicta odio natus hic.
+            </p>
+          </article>
+          <img src={millerMap} alt='miller library map'></img>
         </main>
       </section>
     </div>
