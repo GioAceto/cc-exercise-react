155d269962e9b2447461529c45a4523c6699cc91
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-16 11:28:05 -0500

    create countdown timer layout

--- client/src/components/Countdown.js
+++ client/src/components/Countdown.js
@@ -3,7 +3,27 @@ import React from 'react'
 const Countdown = () => {
   return (
     <div className='countdown-container'>
-      <h2>May 22 - 24, 2022</h2>
+      <div className='text-box'>
+        <h2>May 22 - 24, 2022</h2>
+      </div>
+      <div className='timer-container'>
+        <div className='timer-box'>
+          <p>60</p>
+          <p>days</p>
+        </div>
+        <div className='timer-box'>
+          <p>60</p>
+          <p>hours</p>
+        </div>
+        <div className='timer-box'>
+          <p>60</p>
+          <p>minutes</p>
+        </div>
+        <div className='timer-box'>
+          <p>60</p>
+          <p>seconds</p>
+        </div>
+      </div>
     </div>
   )
 }
