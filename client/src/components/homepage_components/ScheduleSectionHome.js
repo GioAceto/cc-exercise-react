50f7e5480fa74f80abf1bed47209be4b09b6522f
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-17 09:42:06 -0500

    add link to colby events page to the ScheduleSectionHome component

--- client/src/components/homepage_components/ScheduleSectionHome.js
+++ client/src/components/homepage_components/ScheduleSectionHome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import EventsList from "../shared_components/EventsList";
+import calendar from '../../assets/svg/calendar.svg';
 
 const ScheduleSectionHome = () => {
   return (
@@ -7,6 +8,9 @@ const ScheduleSectionHome = () => {
       <header>
         <h2>Schedule</h2>
       </header>
+      <a href='https://events.colby.edu' target='_blank' rel='noreferrer'>
+        <img className='events-calendar-icon' src={calendar} alt='link to events calendar' />
+      </a>
       <EventsList />
       <button className='load-more-btn'>load more</button>
     </section>
