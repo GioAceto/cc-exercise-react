fffe16fcc73c257c473d120da425b61efa556578
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-17 14:00:47 -0500

    integrate juicer-feed to the ShareSectionHome component

--- client/src/components/homepage_components/ShareSectionHome.js
+++ client/src/components/homepage_components/ShareSectionHome.js
@@ -1,7 +1,7 @@
+import React, { useEffect } from 'react';
 import fb from '../../assets/svg/fb.svg';
 import twit from '../../assets/svg/twit.svg';
 import insta from '../../assets/svg/insta.svg';
-
 
 const ShareSectionHome = () => {
   return (
@@ -9,9 +9,10 @@ const ShareSectionHome = () => {
       <header>
         <h2>Share</h2>
       </header>
-      <button className='load-more-btn'>load more</button>
+      <ul className="juicer-feed" data-feed-id="colby-college-master-feed" data-per="8"></ul>
+      {/* <button className='load-more-btn'>load more</button> */}
       <h3>@colbycollege</h3>
-      <ul class='social-container'>
+      <ul className='social-container'>
         <li><a href='https://www.facebook.com/colbycollege/' target='_blank' rel='noreferrer'><img src={fb} alt='facebook' /></a></li>
         <li><a href='https://www.instagram.com/colbycollege' target='_blank' rel='noreferrer'><img src={twit} alt='twitter' /></a></li>
         <li><a href='https://twitter.com/ColbyCollege' target='_blank' rel='noreferrer'><img src={insta} alt='instagram' /></a></li>
