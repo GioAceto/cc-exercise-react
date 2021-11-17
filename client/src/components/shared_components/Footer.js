55f7247a7f3f2f540631325056ed01e1ef84f8b2
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-17 12:48:45 -0500

    add logo and footer links to Footer component

--- client/src/components/shared_components/Footer.js
+++ client/src/components/shared_components/Footer.js
@@ -1,8 +1,21 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import logo from '../../assets/images/colby_logo.png';
+
 const Footer = () => {
   return (
-    <div>
-
-    </div>
+    <footer>
+      <div>
+        <img className='logo' src={logo} alt="Colby logo"></img>
+        <ul className='nav-links-container'>
+          <li><Link to='/' className='nav-link' href='#'>Home</Link></li>
+          <li><Link to='/schedule' className='nav-link' href='#'>Schedule</Link></li>
+          <li><Link to='/registration' className='nav-link' href='#'>Registration</Link></li>
+          <li><Link to='/share' className='nav-link' href='#'>Share</Link></li>
+          <li><Link to='/galleries' className='nav-link' href='#'>Galleries</Link></li>
+        </ul>
+      </div>
+    </footer>
   )
 }
 
