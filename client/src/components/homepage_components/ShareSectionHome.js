7d2224a0afb9adf36e7b85da669667b31e501f48
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-17 11:37:42 -0500

    add load more buttons to homepage

--- client/src/components/homepage_components/ShareSectionHome.js
+++ client/src/components/homepage_components/ShareSectionHome.js
@@ -1,8 +1,17 @@
 const ShareSectionHome = () => {
   return (
-    <div>
-
-    </div>
+    <section className='share-section'>
+      <header>
+        <h2>Share</h2>
+      </header>
+      <button className='load-more-btn'>load more</button>
+      <h3>@colbycollege</h3>
+      <div class='social-container'>
+        <a href='https://www.facebook.com/colbycollege/' target='_blank' rel='noreferrer'><img src={ } alt='facebook'></img></a>
+        <a href='https://www.instagram.com/colbycollege' target='_blank' rel='noreferrer'><img src={ } alt='twitter'></img></a>
+        <a href='https://twitter.com/ColbyCollege' target='_blank' rel='noreferrer'><img src={ } alt='instagram'></img></a>
+      </div>
+    </section>
   )
 }
 
