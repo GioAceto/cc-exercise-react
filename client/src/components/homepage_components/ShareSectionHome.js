3a8df3daa6f07f8aef5fa91271a79aa16045b3aa
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-17 11:58:46 -0500

    add social icon links to ShareSectionHome component

--- client/src/components/homepage_components/ShareSectionHome.js
+++ client/src/components/homepage_components/ShareSectionHome.js
@@ -1,3 +1,8 @@
+import fb from '../../assets/svg/fb.svg';
+import twit from '../../assets/svg/twit.svg';
+import insta from '../../assets/svg/insta.svg';
+
+
 const ShareSectionHome = () => {
   return (
     <section className='share-section'>
@@ -6,11 +11,11 @@ const ShareSectionHome = () => {
       </header>
       <button className='load-more-btn'>load more</button>
       <h3>@colbycollege</h3>
-      <div class='social-container'>
-        <a href='https://www.facebook.com/colbycollege/' target='_blank' rel='noreferrer'><img src={ } alt='facebook'></img></a>
-        <a href='https://www.instagram.com/colbycollege' target='_blank' rel='noreferrer'><img src={ } alt='twitter'></img></a>
-        <a href='https://twitter.com/ColbyCollege' target='_blank' rel='noreferrer'><img src={ } alt='instagram'></img></a>
-      </div>
+      <ul class='social-container'>
+        <li><a href='https://www.facebook.com/colbycollege/' target='_blank' rel='noreferrer'><img src={fb} alt='facebook' /></a></li>
+        <li><a href='https://www.instagram.com/colbycollege' target='_blank' rel='noreferrer'><img src={twit} alt='twitter' /></a></li>
+        <li><a href='https://twitter.com/ColbyCollege' target='_blank' rel='noreferrer'><img src={insta} alt='instagram' /></a></li>
+      </ul>
     </section>
   )
 }
