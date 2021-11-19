2ce3cc95093529d73d0bb6bb64e231285b23ca08
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-18 22:02:26 -0500

    fix footer class names

--- client/src/components/shared_components/Footer.js
+++ client/src/components/shared_components/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <img className='logo' src={logo} alt="Colby logo"></img>
-        <ul className='nav-links-container'>
-          <li><Link to='/' className='nav-link' href='#'>Home</Link></li>
-          <li><Link to='/schedule' className='nav-link' href='#'>Schedule</Link></li>
-          <li><Link to='/registration' className='nav-link' href='#'>Registration</Link></li>
-          <li><Link to='/share' className='nav-link' href='#'>Share</Link></li>
-          <li><Link to='/galleries' className='nav-link' href='#'>Galleries</Link></li>
+        <img className='footer-logo' src={logo} alt="Colby logo"></img>
+        <ul className='footer-links-container'>
+          <li><Link to='/' className='footer-link' href='#'>Home</Link></li>
+          <li><Link to='/schedule' className='footer-link' href='#'>Schedule</Link></li>
+          <li><Link to='/registration' className='footer-link' href='#'>Registration</Link></li>
+          <li><Link to='/share' className='footer-link' href='#'>Share</Link></li>
+          <li><Link to='/galleries' className='footer-link' href='#'>Galleries</Link></li>
         </ul>
       </div>
     </footer>
