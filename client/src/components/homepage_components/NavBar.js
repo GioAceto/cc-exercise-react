3521112aac0e8dd86c27703b3076a8826af3f26d
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-11-18 20:32:48 -0500

    add mobile menu to NavBar component

--- client/src/components/homepage_components/NavBar.js
+++ client/src/components/homepage_components/NavBar.js
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import logo from '../../assets/images/colby_logo.png';
 import home from '../../assets/svg/home.svg';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { IconContext } from 'react-icons';
 
 const NavBar = () => {
   const [navBar, setNavBar] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
 
   const changeBackground = () => {
     if (window.scrollY >= 120) {
@@ -17,21 +23,43 @@ const NavBar = () => {
   window.addEventListener('scroll', changeBackground)
 
   return (
-    <div className={navBar ? 'nav-container active' : 'nav-container'}>
-      <nav>
-        <img className='logo' src={logo} alt="Colby logo"></img>
-        <ul className='nav-links-container'>
-          <li><Link to='/' className='nav-link' href='#'>Home</Link></li>
-          <li><Link to='/schedule' className='nav-link' href='#'>Schedule</Link></li>
-          <li><Link to='/registration' className='nav-link' href='#'>Registration</Link></li>
-          <li><Link to='/share' className='nav-link' href='#'>Share</Link></li>
-          <li><Link to='/galleries' className='nav-link' href='#'>Galleries</Link></li>
-          <li>
-            <Link to='/'><img className='home-icon-link' src={home} alt="Home icon"></img></Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className={navBar ? 'navbar active' : 'navbar'}>
+          <img className='logo' src={logo} alt="Colby logo"></img>
+          <ul className='nav-links-container'>
+            <li><Link to='/' className='nav-link' href='#'>Home</Link></li>
+            <li><Link to='/schedule' className='nav-link' href='#'>Schedule</Link></li>
+            <li><Link to='/registration' className='nav-link' href='#'>Registration</Link></li>
+            <li><Link to='/share' className='nav-link' href='#'>Share</Link></li>
+            <li><Link to='/galleries' className='nav-link' href='#'>Galleries</Link></li>
+            <li>
+              <Link to='/'><img className='home-icon-link' src={home} alt="Home icon"></img></Link>
+            </li>
+          </ul>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars close'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            <li className='nav-text'><Link to='/' className='nav-link' href='#'>Home</Link></li>
+            <li className='nav-text'><Link to='/schedule' className='nav-link' href='#'>Schedule</Link></li>
+            <li className='nav-text'><Link to='/registration' className='nav-link' href='#'>Registration</Link></li>
+            <li className='nav-text'><Link to='/share' className='nav-link' href='#'>Share</Link></li>
+            <li className='nav-text'><Link to='/galleries' className='nav-link' href='#'>Galleries</Link></li>
+            <li className='nav-text'>
+              <Link to='/'><img className='home-icon-link' src={home} alt="Home icon"></img></Link>
+            </li>
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
   )
 }
 
--- client/src/components/homepage_components/ShareSectionHome.js
+++ client/src/components/homepage_components/ShareSectionHome.js
@@ -16,8 +16,8 @@ const ShareSectionHome = () => {
       <h3>@colbycollege</h3>
       <ul className='social-container'>
         <li><a href='https://www.facebook.com/colbycollege/' target='_blank' rel='noreferrer'><img src={fb} alt='facebook' /></a></li>
-        <li><a href='https://www.instagram.com/colbycollege' target='_blank' rel='noreferrer'><img src={twit} alt='twitter' /></a></li>
-        <li><a href='https://twitter.com/ColbyCollege' target='_blank' rel='noreferrer'><img src={insta} alt='instagram' /></a></li>
+        <li><a href='https://twitter.com/ColbyCollege' target='_blank' rel='noreferrer'><img src={twit} alt='twitter' /></a></li>
+        <li><a href='https://www.instagram.com/colbycollege' target='_blank' rel='noreferrer'><img src={insta} alt='instagram' /></a></li>
       </ul>
     </section>
   )
